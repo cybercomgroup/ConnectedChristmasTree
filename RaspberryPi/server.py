0595921ed03cf3c110b3a2a518d5f23e34b2f4f6
Taner Yavuz <taner.yavuz@cybercom.com>
2017-01-12 13:19:25 +0100

    Improving code with error returns

--- RaspberryPi/server.py
+++ RaspberryPi/server.py
@@ -3,19 +3,43 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 def change_led_color(colors):
-        if blinkstick.find_all() > 0:
-                print 'Changing colors to: ', colors[0:3]   
-                for ledstrip in blinkstick.find_all():
-                        for x in range(0, 32):
-                                ledstrip.set_color(0, x ,int(colors[0]),int(colors[1]),int(colors[2]))
-                return 'ok'
+    bsticks = blinkstick.find_all()
+    if len(bsticks) > 0:
+        print 'Changing colors to: ', colors[0:3]
+        for ledstrip in bsticks:
+            for x in range(0, 32):
+                ledstrip.set_color(channel=0, index=x, red=int(colors[0]), green=int(colors[1]), blue=int(colors[2]))
+        return 'ok'
+    else:
+        print 'No blinkstick found'
+        return 'No blinkstick found'
+
+
+def change_led_random():
+    bsticks = blinkstick.find_all()
+    if len(bsticks) > 0:
+        print 'Setting a random color to all leds'
+        for bstick in bsticks:
+            for x in range(0, 32):
+                bstick.set_color(channel=0, index=x, name="random")
+        return 'ok'
+    else:
+        print 'No blinkstick found'
+        return 'No blinkstick found'
+
 
 @app.route('/changecolor', methods=['POST'])
 def change_color_all():
-        payload = request.get_json()
-        change_led_color(payload['rgb'])
-        return ''
+    payload = request.get_json()
+    return change_led_color(payload['rgb'])
+
+
+@app.route('/randomcolor', methods=['GET'])
+def random_color_all():
+    return change_led_random()
+
 
 if __name__ == '__main__':
-                app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')

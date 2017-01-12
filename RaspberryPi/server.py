85d217ac5052248218314f0690596c594b1cd9fc
Taner Yavuz <taner.yavuz@cybercom.com>
2017-01-12 13:37:36 +0100

    Added check for cpu usage

--- RaspberryPi/server.py
+++ RaspberryPi/server.py
@@ -1,5 +1,6 @@
 from blinkstick import blinkstick
 from flask import Flask, jsonify, request
+import psutil
 
 app = Flask(__name__)
 
@@ -8,9 +9,10 @@ def change_led_color(colors):
     bsticks = blinkstick.find_all()
     if len(bsticks) > 0:
         print 'Changing colors to: ', colors[0:3]
-        for ledstrip in bsticks:
-            for x in range(0, 32):
-                ledstrip.set_color(channel=0, index=x, red=int(colors[0]), green=int(colors[1]), blue=int(colors[2]))
+        for bstick in bsticks:
+            ledCount = bstick.get_led_count()
+            for x in range(0, ledCount):
+                bstick.set_color(channel=0, index=x, red=int(colors[0]), green=int(colors[1]), blue=int(colors[2]))
         return 'ok'
     else:
         print 'No blinkstick found'
@@ -22,12 +24,16 @@ def change_led_random():
     if len(bsticks) > 0:
         print 'Setting a random color to all leds'
         for bstick in bsticks:
-            for x in range(0, 32):
+            ledCount = bstick.get_led_count()
+            for x in range(0, ledCount):
                 bstick.set_color(channel=0, index=x, name="random")
         return 'ok'
     else:
         print 'No blinkstick found'
         return 'No blinkstick found'
+
+def cpu_usage():
+    return str(psutil.cpu_percent(interval=1)) + "%"
 
 
 @app.route('/changecolor', methods=['POST'])
@@ -39,6 +45,10 @@ def change_color_all():
 @app.route('/randomcolor', methods=['GET'])
 def random_color_all():
     return change_led_random()
+
+@app.route('/getcpuusage', methods=['GET'])
+def get_cpu_usage():
+    return cpu_usage()
 
 
 if __name__ == '__main__':

0df3028261eba7c368a3a67c45ec90b0576f567a
Taner Yavuz <taner.yavuz@cybercom.com>
2017-01-16 08:38:54 +0100

    Added spaz out function and first attempt at async calls
    Note. Need to improve the async with better implementation.
    E.g How to stop function call when calling same function?

--- RaspberryPi/server.py
+++ RaspberryPi/server.py
@@ -1,9 +1,12 @@
 from blinkstick import blinkstick
 from flask import Flask, jsonify, request
+import threading
 import psutil
+import random
 
 app = Flask(__name__)
 
+is_looping = True
 
 def change_led_color(colors):
     bsticks = blinkstick.find_all()
@@ -33,7 +36,23 @@ def change_led_random():
         return 'No blinkstick found'
 
 
+def spaz_out():
+    bsticks = blinkstick.find_all()
+    if len(bsticks) > 0:
+        print 'Setting a random color to all leds'
+        while(is_looping):
+            for bstick in bsticks:
+                led_count = bstick.get_led_count()
+                random_led = random.randint(0, 32)
+                bstick.set_color(channel=0, index=random_led, name="random")
+        return 'ok'
+    else:
+        print 'No blinkstick found'
+        return 'No blinkstick found'
+
+
 def cpu_usage():
+    print 'Returning cpu usage'
     return str(psutil.cpu_percent(interval=1))
 
 
@@ -44,25 +63,41 @@ def connections():
 
 
 @app.route('/changecolor', methods=['POST'])
+
 def change_color_all():
+    global is_looping
+    is_looping = False
     payload = request.get_json()
     return change_led_color(payload['rgb'])
 
 
-@app.route('/randomcolor', methods=['GET'])
+@app.route('/randomcolor', methods=['POST'])
 def random_color_all():
+    global is_looping
+    is_looping = False
     return change_led_random()
 
 
-@app.route('/getcpuusage', methods=['GET'])
+@app.route('/getcpuusage', methods=['POST'])
 def get_cpu_usage():
+    global is_looping
+    is_looping = False
     return cpu_usage()
 
 
-@app.route('/getconnections', methods=['GET'])
+@app.route('/getconnections', methods=['POST'])
 def get_connections():
+    global is_looping
+    is_looping = False
     return connections()
 
 
+@app.route('/randomblink', methods=['POST'])
+def spazing_out():
+    global is_looping
+    is_looping = True
+    return spaz_out()
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', threaded=True)

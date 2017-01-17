bcbd7ad39b460e59bbde8f2a928f5b27c92079d9
Taner Yavuz <taner.yavuz@cybercom.com>
2017-01-17 16:01:38 +0100

    Fixed spaz out function

--- RaspberryPi/server.py
+++ RaspberryPi/server.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 spazing_out = True
 
+
 class Main(blinkstick.BlinkStickPro):
     def change_led_color(self, colors):
         self.clear()
@@ -15,7 +16,7 @@ class Main(blinkstick.BlinkStickPro):
         for x in range(0, self.r_led_count):
             self.set_color_or(x, int(colors[0]), int(colors[1]), int(colors[2]))
         self.send_data_all()
-        time.sleep(0.1)
+        time.sleep(0.020)
         return 'ok'
 
     def change_led_random(self):
@@ -28,25 +29,27 @@ class Main(blinkstick.BlinkStickPro):
             self.set_color_or(x, rr, rb, rg)
 
         self.send_data_all()
-        time.sleep(0.1)
+        time.sleep(0.020)
         return 'ok'
 
-    def spaz_out(self): # Needs a bit of update
-        print 'Setting a random color to all leds on random index'
+    def spaz_out(self):  # Needs a bit of update
+        print 'Setting a random color to a led on random index'
         while spazing_out:
+            random_led = random.randint(0, self.r_led_count-1)
+
             rr = random.randint(0, 255)
             rg = random.randint(0, 255)
             rb = random.randint(0, 255)
-            random_led = random.randint(0, self.r_led_count)
 
             self.set_color_or(random_led, rr, rb, rg)
             self.send_data_all()
-            time.sleep(0.1)
+            time.sleep(0.020)
         return 'ok'
 
     def set_color_or(self, x, r, g, b):
-        cr, cg, cb = self.get_color(0, x)
-        self.set_color(0, x, int(r) | int(cr), int(g) | int(cg), int(b) | int(cb))
+        #cr, cg, cb = self.get_color(0, x)
+        #self.set_color(0, x, int(r) | int(cr), int(g) | int(cg), int(b) | int(cb))
+        self.set_color(0, x, int(r), int(g), int(b))
 
 bstick = Main(r_led_count=32)
 if not bstick.connect():
@@ -100,6 +103,6 @@ def spazing_out():
     spazing_out = True
     return bstick.spaz_out()
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
-

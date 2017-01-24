5103ce114554ec6f6235ac5ff5f03d895fbbbf51
Taner Yavuz <taner.yavuz@cybercom.com>
2017-01-24 14:18:14 +0100

    Made it possible to send response when running spaz_out
    Running a loop in a another thread and setting a bool to True
    or false to run the spaz_out

--- RaspberryPi/Bstick/Bstick.py
+++ RaspberryPi/Bstick/Bstick.py
@@ -2,12 +2,12 @@ from blinkstick import blinkstick
 import random
 import time
 
-spazing_out = True
 
 class Stick(blinkstick.BlinkStickPro):
+
     def change_led_color(self, colors):
         self.clear()
-        print 'Changing colors to: ', colors[0:3]
+        print ('Changing colors to: ', colors[0:3])
         for x in range(0, self.r_led_count):
             self.set_color_or(x, int(colors[0]), int(colors[1]), int(colors[2]))
         self.send_data_all()
@@ -16,7 +16,7 @@ class Stick(blinkstick.BlinkStickPro):
 
     def change_led_random(self):
         self.clear()
-        print 'Setting a random color to all leds'
+        print ('Setting a random color to all leds')
         for x in range(0, self.r_led_count):
             rr = random.randint(0, 255)
             rg = random.randint(0, 255)
@@ -27,19 +27,16 @@ class Stick(blinkstick.BlinkStickPro):
         time.sleep(0.020)
         return 'ok'
 
-    def spaz_out(self):  # Needs a bit of update
-        print 'Setting a random color to a led on random index'
-        while spazing_out:
-            random_led = random.randint(0, self.r_led_count-1)
+    def spaz_out(self):
+        random_led = random.randint(0, self.r_led_count-1)
 
-            rr = random.randint(0, 255)
-            rg = random.randint(0, 255)
-            rb = random.randint(0, 255)
+        rr = random.randint(0, 255)
+        rg = random.randint(0, 255)
+        rb = random.randint(0, 255)
 
-            self.set_color_or(random_led, rr, rb, rg)
-            self.send_data_all()
-            time.sleep(0.003)
-        return 'ok'
+        self.set_color_or(random_led, rr, rb, rg)
+        self.send_data_all()
+        time.sleep(0.003)
 
     def set_color_or(self, x, r, g, b):
         #cr, cg, cb = self.get_color(0, x)
--- RaspberryPi/server.py
+++ RaspberryPi/server.py
@@ -1,17 +1,23 @@
 from Bstick import Bstick
 from flask import Flask, jsonify, request
 import psutil
+import threading
 
 app = Flask(__name__)
 
-bstick = Bstick.Stick(r_led_count=32)
+spazing_out_flag = False
+run_loop = True
+
+bstick = Bstick.Stick(r_led_count=32, max_rgb_value=100)
 if not bstick.connect():
-    print "No blinkstick found"
+    print ("No blinkstick found")
     exit()
+else:
+    bstick.clear()
 
 
 def cpu_usage():
-    print 'Returning cpu usage'
+    print ('Returning cpu usage')
     return str(psutil.cpu_percent(interval=1))
 
 
@@ -23,34 +29,49 @@ def connections():
 
 @app.route('/changecolor', methods=['POST'])
 def change_color_all():
-    Bstick.spazing_out = False
+    global spazing_out_flag
+    spazing_out_flag = False
     payload = request.get_json()
     return bstick.change_led_color(payload['rgb'])
 
 
 @app.route('/randomcolor', methods=['POST'])
 def random_color_all():
-    Bstick.spazing_out = False
+    global spazing_out_flag
+    spazing_out_flag = False
     return bstick.change_led_random()
 
 
 @app.route('/getcpuusage', methods=['POST'])
 def get_cpu_usage():
-    Bstick.spazing_out = False
     return cpu_usage()
 
 
 @app.route('/getconnections', methods=['POST'])
 def get_connections():
-    Bstick.spazing_out = False
     return connections()
-
 
 @app.route('/randomblink', methods=['POST'])
 def spazing_out():
-    Bstick.spazing_out = True
-    return bstick.spaz_out()
+    global spazing_out_flag
+    spazing_out_flag = True
+    return 'ok'
 
+def run_spaz_loop():
+    global spazing_out_flag
+    global run_loop
+    while run_loop:
+        if spazing_out_flag == True:
+            bstick.spaz_out()
+
+
+def stop_loop():
+    global run_loop
+    run_loop = False
 
 if __name__ == '__main__':
+    t = threading.Thread(target=run_spaz_loop)
+    t.start()
     app.run(host='0.0.0.0', threaded=True)
+    stop_loop()
+    t.join()
